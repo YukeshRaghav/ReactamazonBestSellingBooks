18ba982549961ae942cbfe2d211974c78b5432c8
Yukesh Raghavan <yukeshraghavan@Yukeshs-MacbookPro.local>
2023-10-13 20:52:29 -0400

    first commit

---
+++ src/books.js
@@ -0,0 +1,23 @@
+import img1 from './image/book1.jpg';
+import img2 from './image/book2.jpg';
+import img3 from './image/book3.jpg'
+export const books = [
+    {
+      author: 'Rebecca Yarros ',
+      title: 'Fourth Wing (Special Edition) (The Empyrean, 1)',
+      img: img1,
+      id: 1,
+    },
+    {
+      author: 'Rebecca Yarros ',
+      title: 'Iron Flame (The Empyrean, 2)',
+      img: img2,
+      id: 2,
+    },
+    {
+        author: 'Adam Wallace ',
+        title: 'How to Catch a Monster: A Halloween Picture Book for Kids About Conquering Fears!',
+        img: img3,
+        id: 3,
+      },
+  ];
